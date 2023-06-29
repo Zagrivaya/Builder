6967a52fdd32a7a52a0118104e5d9f30eb0088ee
Zagrivaya <zagrivaya@yandex.ru>
2023-06-29 19:49:07 +0300

    init commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        Person mom = new Person.Builder()
+                .setName("Анна")
+                .setSurname("Вольф")
+                .setAge(21)
+                .setAddress("Сидней")
+                .build();
+
+        Person son = mom.newChildBuilder()
+                .setName("Антошка")
+                .build();
+
+        System.out.println("У " + mom + " есть сын, " + son);
+    }
+}
